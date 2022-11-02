facb21a7444c406649dbe75e6442b28c9e277aef
Omar.Abdelaziz <46730385+OmarAbdelazizMahmed@users.noreply.github.com>
2022-11-02 04:25:29 +0200

    Added top sellings

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -15,10 +15,14 @@ class WelcomeController extends Controller
         $categories = Category::select('name', 'slug')->take(4)->get();
         $sliders = Category::select('name', 'slug', 'image')->take(4)->get();
         $featured = Product::where('featured', true)->take(4)->select('name', 'slug', 'main_image')->get();
+        // top selling products are 4 random products
+        $topSelling = Product::inRandomOrder()->take(4)->select('name', 'slug', 'main_image')->get();
         return Inertia::render('Welcome', [
             'featured' => $featured,
             'categories' => $categories,
-            'sliders' => $sliders
+            'sliders' => $sliders,
+            'topSelling' => $topSelling,
+
         ]);
     }
 }
--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -19,6 +19,10 @@ defineProps({
         type: Array,
         default: () => [],
     },
+    topSelling: {
+        type: Array,
+        default: () => [],
+    },
 });
 
 </script>
@@ -44,26 +48,32 @@ defineProps({
 
 
         </div>
-        <!-- <div class="text-center border-t border-b py-4">
-            <p>Lorem ipsum dolor sit amet.</p>
-        </div> -->
+        <!-- top selling products -->
         <div class="flex flex-col max-w-7xl mx-auto px-4 sm:container sm:flex-row sm:space-x-4 sm:my-4 sm:px-6 lg:px-8">
-            <Link :href="route('shop.show', feat.slug)" class="flex flex-col w-full h-full mb-4" v-for="(feat, index) in featured" :key="index">
-                <div class="text-center text-white bg-gray-700 py-2">
-                    <span>
-                        {{ feat.name }}
-                    </span>
+            <div class="w-full">
+                <h1 class="text-2xl font-bold text-gray-900">Top Selling Products</h1>
+                <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+                    <div v-for="(product, index) in topSelling" :key="index" class="w-full">
+                        <div class="bg-white shadow-md rounded-md overflow-hidden">
+                            <div class="w-full h-64">
+                                <img :src="'/storage/' + product.main_image" alt="product image" class="w-full h-full object-cover">
+                            </div>
+                            <div class="p-4">
+                                <h1 class="text-lg font-bold text-gray-900">{{ product.name }}</h1>
+                                <p class="text-gray-600 text-sm">{{ product.description }}</p>
+                                <p class="text-gray-600 text-sm">{{ product.price }}</p>
+                                <MainButton class="mt-4" :href="route('shop.show', product.slug)">View Product</MainButton>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <img :src="'/storage/' + feat.main_image" :alt="feat.name" class="h-72 object-cover md:w-72 lg:w-96">
-            </Link>
+            </div>
         </div>
-        <div class="text-center text-white bg-black px-4 py-4">
-            <p class="container">
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iusto fuga neque culpa illum sit exercitationem enim dolores dolor quaerat quos, ex ipsa, recusandae in ad voluptates officia voluptatem, nobis animi!
-                Magni harum quos voluptas ex eveniet inventore rerum ducimus repellendus! Saepe, nam. Tempora optio ratione ipsa. Commodi est voluptatem excepturi dignissimos voluptas nostrum ut atque, repellendus quidem nesciunt, maiores fugiat?
-                Esse eligendi amet quo error, nostrum necessitatibus laudantium nulla rem quidem accusantium tenetur, aperiam qui excepturi! Natus, praesentium repellat esse optio fugiat id vel voluptatum deserunt minus, provident cum doloribus.
-            </p>
-        </div>
+        <!-- featured products would be a slider -->
+
+
+
+
     </AppLayout>
 </template>
 
